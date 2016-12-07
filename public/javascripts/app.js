7be6a5655c3281c54157e0ee406b640d47c400da
Jesus Ramos <jro867@gmail.com>
2016-12-07 11:43:00 -0700

    authentication working

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,6 +3,9 @@ angular.module('blogger', [])
   '$scope','$http',
   function($scope,$http){
    //  $scope.test = 'Hello world!';
+   $scope.addContentButton = false;
+   $scope.deletePostButton = false;
+
 	  $scope.contents = [
       {
         title:'Bringing Firebase To Your Server',
@@ -12,6 +15,21 @@ angular.module('blogger', [])
       {title:'MongoDB launches Atlas, its new database-as-a-service offering', picture:"http://blogs-images.forbes.com/adrianbridgwater/files/2016/06/1mongo.jpg?width=960", content:"MongoDB, the company behind the eponymous open source database, is launching Atlas today, its third major revenue-generating service. Atlas is MongoDB’s database-as-a-service offering that provides users with a managed database service. The service will offer pay-as-you-go pricing and will initially allow users to deploy on Amazon Web Services (AWS), with support for Microsoft Azure and Google Cloud Platform coming later. MongoDB Atlas complements the company’s commercial offering for enterprises who want to run the service on-premise, and MongoDB Professional, which provides businesses with support and access to the company’s Cloud Manager and other tools. Atlas fits somewhere between these two services. It allows anybody who wants to use MongoDB to quickly provision it in the cloud, get support, and only pay an hourly fee. MongoDB’s VP of Strategy Kelly Stirman tells me that he believes most developers love MongoDB because it makes them productive (though it’s worth noting that there are, of course, also plenty of developers who don’t love MongoDB). MongoDB is great for developing the application, he said. But when it comes to deploying the application in production, that’s where developers can get stuck. With this, we took all the experience we have supporting large systems and deployments in production — and the software we developed to help people. As a user, you don’t need to know much more than where you want to deploy and what size database you need."},
       {title:"Using Angular CLI to create Angular 2 applications in Docker", picture:"https://yellowpencil.com/assets/blog/banners/banner-angularjs.jpg", content:"Angular CLI is a great tool for developing  Angular 2 applications.  Now that the final 2.0 release is out and the masses are starting to be interested I thought it would be fun to do a quick demo of how you can try out the Angular CLI (github) project without having to install all the tools on your local machine.  The caveat there is that I assume you already have Docker installed. In that file we specify we want to use the current Node.js image as our base. Then we setup our home directory so our code will have a place to live. Next we use NPM (Node Package Manager) to install the Angular CLI tool chain. Then we expose the default port the Angular 2 project will be served to the browser from, and we open up the port that will be used for live reloading of code. Next we want to create a docker-compose.yml file which will define our service we will use to execute the build commands and run our development web server iniside."},
     ];
+
+    $scope.userLogin = function(){
+      console.log("loggin in angular");
+      $scope.addContentButton = true;
+      $scope.deletePostButton = true;
+    }
+
+    $scope.userLogout = function(){
+      console.log("loggout in angular");
+      $scope.addContentButton = false;
+      $scope.deletePostButton = false;
+    }
+
+
+
     $scope.create = function(data){
       return $http.post('/content', data).success(function(data){
         // console.log("Blame thing: ", data);
@@ -44,7 +62,7 @@ angular.module('blogger', [])
 
     }
 
-	
+
   };
 
   $scope.deleteContent = function(index){
